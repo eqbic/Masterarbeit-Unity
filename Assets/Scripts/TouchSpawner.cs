ca9bcc96651734596ad4f7a7e9eb33dc176b53d1
Erich Querner <querner@interactive-scape.com>
2024-08-16 10:44:52 +0200

    separate touch spawn logic

---
+++ Assets/Scripts/TouchSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+
+public class TouchSpawner : MonoBehaviour
+{
+    [SerializeField] private TapGesture _tapGesture;
+    [SerializeField] private ViewFinderSpawner _spawner;
+
+    private uint _currentId = 0;
+    
+    private void OnEnable()
+    {
+        _tapGesture.Tapped += SpawnViewFinder;
+    }
+
+    private void OnDisable()
+    {
+        _tapGesture.Tapped -= SpawnViewFinder;
+    }
+
+    private void SpawnViewFinder(object sender, EventArgs e)
+    {
+        _spawner.SpawnViewFinder(_currentId, _tapGesture.ScreenPosition);
+        _currentId += 1;
+    }
+}
