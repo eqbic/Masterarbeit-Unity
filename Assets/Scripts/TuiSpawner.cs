b3cd915e1b1e8ee098a6c3ee518124d7211bf47c
Erich Querner <querner@interactive-scape.com>
2024-08-16 10:46:00 +0200

    add script to spawn viewfinder with tuio objects

---
+++ Assets/Scripts/TuiSpawner.cs
@@ -0,0 +1,37 @@
+﻿using TuioNet.Tuio20;
+using TuioUnity.Common;
+using UnityEngine;
+
+public class TuiSpawner : MonoBehaviour
+{
+    [SerializeField] private TuioSessionBehaviour _tuioSession;
+    [SerializeField] private ViewFinderSpawner _viewFinderSpawner;
+
+    private Tuio20Dispatcher Dispatcher => (Tuio20Dispatcher)_tuioSession.TuioDispatcher;
+
+    private void OnEnable()
+    {
+        Dispatcher.OnObjectAdd += SpawnViewFinder;
+        Dispatcher.OnObjectRemove += DestroyViewFinder;
+    }
+
+    private void OnDisable()
+    {
+        Dispatcher.OnObjectAdd -= SpawnViewFinder;
+        Dispatcher.OnObjectRemove -= DestroyViewFinder;
+    }
+
+    private void SpawnViewFinder(object sender, Tuio20Object tuioObject)
+    {
+        if (!tuioObject.ContainsTuioToken()) return;
+        var position = TuioUtils.ToScreenPoint(tuioObject.Token.Position);
+        _viewFinderSpawner.SpawnViewFinder(tuioObject.Token.SessionId,position);
+    }
+
+    private void DestroyViewFinder(object sender, Tuio20Object tuioObject)
+    {
+        if (!tuioObject.ContainsTuioToken()) return;
+        var id = tuioObject.Token.SessionId;
+        _viewFinderSpawner.DestroyViewFinder(id);
+    }
+}
