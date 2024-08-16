91aea80314f547ea31f79054bcdef48eb2a7813c
Erich Querner <querner@interactive-scape.com>
2024-08-16 10:45:39 +0200

    created tuio utils. could be later ported to the tuio client

---
+++ Assets/Scripts/TuioUtils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class TuioUtils
+{
+    public static Vector2 ToScreenPoint(System.Numerics.Vector2 tuioPosition)
+    {
+        var x = tuioPosition.X * Screen.width;
+        var y = (1f - tuioPosition.Y) * Screen.height;
+        return new Vector2(x, y);
+    }
+}
