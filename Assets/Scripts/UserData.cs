f3ab5bcd27cad024e8424383903365c3851c2cbe
Erich Querner <querner@interactive-scape.com>
2024-11-12 13:59:31 +0100

    create user data so

---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New UserData", menuName = "Masterarbeit/New UserData", order = 0)]
+public class UserData : ScriptableObject
+{
+    [field:SerializeField] public int UserId { get; set; }
+}
